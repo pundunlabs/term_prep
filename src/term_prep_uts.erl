646ea6f3ed0ff962f18e6f0b5a7c34748a4dfaa5
erdem <erdem.aksu@mobilearts.com>
2017-11-03 12:30:41 +0100

    Break when text ends with MidNumLet.

--- src/term_prep_uts.erl
+++ src/term_prep_uts.erl
@@ -28,9 +28,9 @@
 -spec word_boundaries(unicode:chardata()) ->
     [unicode:chardata()].
 word_boundaries(Chardata) when is_binary(Chardata) -> %% WB1
-    wb(binary_to_list(Chardata), [], [], undefined);
+    wb(binary_to_list(Chardata), [], [], sot);
 word_boundaries(Chardata) when is_list(Chardata) -> %% WB1
-    wb(Chardata, [], [], undefined).
+    wb(Chardata, [], [], sot).
 
 %%%===================================================================
 %%% Internal functions
@@ -38,67 +38,60 @@ word_boundaries(Chardata) when is_list(Chardata) -> %% WB1
 wb([], Word, Acc, _) -> %% WB2
     lists:reverse(acc_word(Word, Acc));
 wb([13, 10 | Rest], Word,  Acc, _) -> %%WB3
-    wb(Rest, [], acc_word(Word, Acc), 'LF');
+    wb(Rest, [], acc_word(Word, Acc), sot);
 wb([16#000A | Rest], Word,  Acc, _) -> %%WB3a, WB3b
-    wb(Rest, [], acc_word(Word, Acc), 'LF');
+    wb(Rest, [], acc_word(Word, Acc), sot);
 wb([16#000B | Rest], Word,  Acc, _) -> %%WB3a, WB3b
-    wb(Rest, [], acc_word(Word, Acc), 'Newline');
+    wb(Rest, [], acc_word(Word, Acc), sot);
 wb([16#000C | Rest], Word,  Acc, _) -> %%WB3a, WB3b
-    wb(Rest, [], acc_word(Word, Acc), 'Newline');
+    wb(Rest, [], acc_word(Word, Acc), sot);
 wb([16#000D | Rest], Word,  Acc, _) -> %%WB3a, WB3b
-    wb(Rest, [], acc_word(Word, Acc), 'Newline');
+    wb(Rest, [], acc_word(Word, Acc), sot);
 wb([16#0085 | Rest], Word,  Acc, _) -> %%WB3a, WB3b
-    wb(Rest, [], acc_word(Word, Acc), 'Newline');
+    wb(Rest, [], acc_word(Word, Acc), sot);
 wb([16#2028 | Rest], Word,  Acc, _) -> %%WB3a, WB3b
-    wb(Rest, [], acc_word(Word, Acc), 'Newline');
+    wb(Rest, [], acc_word(Word, Acc), sot);
 wb([16#2029 | Rest], Word,  Acc, _) -> %%WB3a, WB3b
-    wb(Rest, [], acc_word(Word, Acc), 'Newline');
-wb([A | Rest], Word,  Acc, undefined) -> %% first char after boundary
+    wb(Rest, [], acc_word(Word, Acc), sot);
+wb([A | Rest], [],  Acc, sot) -> %% first char after boundary
     case term_prep_uwb_lib:property(A) of
-	undefined ->
-	    wb(Rest, Word, Acc, undefined);
-	WB4 when WB4 == 'Extend' orelse
-		 WB4 == 'Format' orelse
-		 WB4 == 'ZWJ' ->
-	    wb(Rest, Word, Acc, WB4);
-	Next ->
-	    wb(Rest, [A | Word], Acc, Next)
-    end;
-wb([A], Word, Acc, Last) ->
-    case {Last, term_prep_uwb_lib:property(A)} of
-	{_, undefined} ->
-	    wb([], Word, Acc, undefined);
-	{_, WB4} when WB4 == 'Extend' orelse
-		      WB4 == 'Format' orelse
-		      WB4 == 'ZWJ' ->
-	    wb([], Word, Acc, WB4);
-	{_, Else} ->
-	    wb([], [A | Word], Acc, Else)
+	E when E == 'ZWJ';
+	       E == 'ALetter';
+	       E == 'Hebrew_Letter';
+	       E == 'Numeric';
+	       E == 'Katakana';
+	       E == 'ExtendNumLet';
+	       E == 'E_Base';
+	       E == 'E_Base_GAZ' ->
+	    wb(Rest, [A], Acc, E);
+	'Regional_Indicator' -> %%WB15
+	    wb(Rest, [A], Acc, 'Regional_Indicator');
+	_ ->
+	    wb(Rest, [], Acc, sot)
     end;
 wb([A | Rest], Word, Acc, Last) ->
-    case {Last, term_prep_uwb_lib:property(A)} of
-	{'ZWJ', 'Glue_After_Zwj'} -> %%WB3c
+    case {Last, term_prep_uwb_lib:property(A), Rest} of
+	{'ZWJ', 'Glue_After_Zwj', _} -> %%WB3c
 	    wb(Rest, [A | Word], Acc, 'Glue_After_Zwj');
-	{'ZWJ', 'E_Base_GAZ'} -> %%WB3c
+	{'ZWJ', 'E_Base_GAZ', _} -> %%WB3c
 	    wb(Rest, [A | Word], Acc, 'E_Base_GAZ');
-	{_, WB4} when WB4 == 'Extend' orelse
+	{_, WB4, _} when WB4 == 'Extend' orelse
 	              WB4 == 'Format' orelse
 		      WB4 == 'ZWJ' -> %%WB4
 	    wb(Rest, Word, Acc, WB4);
-	{'ALetter', 'ALetter'} -> %%WB5
+	{'ALetter', 'ALetter', _} -> %%WB5
 	    wb(Rest, [A | Word], Acc, 'ALetter');
-	{'ALetter', 'Hebrew_Letter'} -> %%WB5
+	{'ALetter', 'Hebrew_Letter', _} -> %%WB5
 	    wb(Rest, [A | Word], Acc, 'Hebrew_Letter');
-	{'Hebrew_Letter', 'ALetter'} -> %%WB5
+	{'Hebrew_Letter', 'ALetter', _} -> %%WB5
 	    wb(Rest, [A | Word], Acc, 'ALetter');
-	{'Hebrew_Letter', 'Hebrew_Letter'} -> %%WB5
+	{'Hebrew_Letter', 'Hebrew_Letter', _} -> %%WB5
 	    wb(Rest, [A | Word], Acc, 'Hebrew_Letter');
-	{AHLetter, WB6} when ((AHLetter == 'ALetter' orelse
-			       AHLetter == 'Hebrew_Letter') andalso
-			      (WB6 == 'MidLetter' orelse
-			       WB6 == 'MidNumLet' orelse
-			       WB6 == 'Single_Quote')) ->
-	    [H|T] = Rest,
+	{AHLetter, WB6, [H|T]} when ((AHLetter == 'ALetter' orelse
+				      AHLetter == 'Hebrew_Letter') andalso
+				     (WB6 == 'MidLetter' orelse
+				      WB6 == 'MidNumLet' orelse
+				      WB6 == 'Single_Quote')) ->
 	    case term_prep_uwb_lib:property(H) of
 		'ALetter' -> %%WB6/7
 		    wb(T, [H, A | Word], Acc, 'ALetter');
@@ -107,56 +100,52 @@ wb([A | Rest], Word, Acc, Last) ->
 		_ ->
 		    wb(Rest, [], acc_word(Word, Acc), WB6)
 	    end;
-	{'Hebrew_Letter', 'Single_Quote'} -> %%WB7a
+	{'Hebrew_Letter', 'Single_Quote', _} -> %%WB7a
 	    wb(Rest, [A | Word], Acc, 'Single_Quote');
-	{'Hebrew_Letter', 'Double_Quote'} ->
-	    [H|T] = Rest,
+	{'Hebrew_Letter', 'Double_Quote', [H|T]} ->
 	    case term_prep_uwb_lib:property(H) of
 		'Hebrew_Letter' ->%%WB7b/c
 		    wb(T, [H, A | Word], Acc, 'Hebrew_Letter');
 		_ ->
 		    wb(Rest, [], acc_word(Word, Acc), 'Double_Quote')
 	    end;
-	{'Numeric', 'Numeric'} -> %WB8
+	{'Numeric', 'Numeric', _} -> %WB8
 	    wb(Rest, [A | Word], Acc, 'Numeric');
-	{AHLetter, 'Numeric'} when AHLetter == 'ALetter';
-	                           AHLetter == 'Hebrew_Letter'-> %WB9
+	{AHLetter, 'Numeric', _} when AHLetter == 'ALetter';
+				      AHLetter == 'Hebrew_Letter'-> %WB9
 	    wb(Rest, [A | Word], Acc, 'Numeric');
-	{'Numeric', AHLetter} when AHLetter == 'ALetter';
-	                           AHLetter == 'Hebrew_Letter'-> %WB10
+	{'Numeric', AHLetter, _} when AHLetter == 'ALetter';
+				      AHLetter == 'Hebrew_Letter'-> %WB10
 	    wb(Rest, [A | Word], Acc, AHLetter);
-	{'Numeric', WB11} when WB11 == 'MidNum' orelse
-	                       WB11 == 'MidNumLet' orelse
-			       WB11 == 'Single_Quote' -> %%WB11/12
-	    [H|T] = Rest,
+	{'Numeric', WB11, [H|T]} when (WB11 == 'MidNum' orelse
+				       WB11 == 'MidNumLet' orelse
+				       WB11 == 'Single_Quote') -> %%WB11/12
 	    case term_prep_uwb_lib:property(H) of
 		'Numeric' ->
 		    wb(T, [H, A | Word], Acc, 'Numeric');
 		_ ->
 		    wb(Rest, [], acc_word(Word, Acc), WB11)
 	    end;
-	{'Katakana', 'Katakana'} -> %% WB13
+	{'Katakana', 'Katakana', _} -> %% WB13
 	    wb(Rest, [A | Word], Acc, 'Katakana');
-	{WB13a, 'ExtendNumLet'} when WB13a == 'ALetter' orelse
-				     WB13a == 'Hebrew_Letter' orelse
-				     WB13a == 'Numeric' orelse
-				     WB13a == 'Katakana' orelse
-				     WB13a == 'ExtendNumLet' -> %%WB13a
+	{WB13a, 'ExtendNumLet', _} when WB13a == 'ALetter' orelse
+					WB13a == 'Hebrew_Letter' orelse
+					WB13a == 'Numeric' orelse
+					WB13a == 'Katakana' orelse
+					WB13a == 'ExtendNumLet' -> %%WB13a
 	    wb(Rest, [A | Word], Acc, 'ExtendNumLet');
-	{'ExtendNumLet', WB13b} when WB13b == 'ALetter'orelse
-				     WB13b == 'Hebrew_Letter'orelse
-				     WB13b == 'Numeric' orelse
-				     WB13b == 'Katakana' -> %%WB13b
+	{'ExtendNumLet', WB13b, _} when WB13b == 'ALetter'orelse
+					WB13b == 'Hebrew_Letter'orelse
+					WB13b == 'Numeric' orelse
+					WB13b == 'Katakana' -> %%WB13b
 	    wb(Rest, [A | Word], Acc, WB13b);
-	{WB14, 'E_Modifier'} when WB14 == 'E_Base' orelse
-				  WB14 == 'E_Base_GAZ' -> %%WB14
+	{WB14, 'E_Modifier', _} when WB14 == 'E_Base' orelse
+				     WB14 == 'E_Base_GAZ' -> %%WB14
 	    wb(Rest, [A | Word], Acc, 'E_Modifier');
-	{sot, 'Regional_Indicator'} -> %%WB15
+	{'Regional_Indicator', 'Regional_Indicator', _} -> %%WB16
 	    wb(Rest, [A | Word], Acc, 'Regional_Indicator');
-	{'Regional_Indicator', 'Regional_Indicator'} -> %%WB16
-	    wb(Rest, [A | Word], Acc, 'Regional_Indicator');
-	{_Last, _Current} ->
-	    wb(Rest, [], acc_word(Word, Acc), undefined)
+	{_Last, _Current, Rest} ->
+	    wb(Rest, [], acc_word(Word, Acc), sot)
     end.
 
 acc_word([], Acc) ->
